1c0f0f456e7a1f3ba3a723c060f2c8fba8a47dcd
zeiod <76787324+weeyc@users.noreply.github.com>
2021-11-07 03:47:44 +0800

    Login v2

--- resources/views/ManageRegistrationUsers/Login.blade.php
+++ resources/views/ManageRegistrationUsers/Login.blade.php
@@ -13,70 +13,84 @@
 
 
 
- <body class="flex items-center justify-center h-screen overflow-hidden bg-gradient-to-r from-green-300 via-blue-500 to-purple-600" >
+ <body class="flex items-center justify-center h-screen overflow-hidden bg-gradient-to-b from-green-200 to-green-500" >
     <body>
-    <section class="flex items-stretch min-h-screen text-white ">
-        <div class="relative items-center hidden w-1/2 bg-gray-500 bg-no-repeat bg-cover lg:flex bg-pekan">
-            <div class="absolute inset-0 z-0 bg-black opacity-5"></div>
-            <div class="z-10 w-full px-24">
-                <h1 class="text-5xl font-bold tracking-wide text-left">Search Accomodation</h1>
-                <p class="my-4 text-3xl">Browse your rental house in more secure way, anywhere.</p>
+    <section class="flex items-stretch h-screen w-screen text-white ">
+        <div class="relative items-center hidden w-1/2 bg-gray-500 bg-no-repeat bg-cover lg:flex" style="background-image: url(/images/UMP/wide.jpg);">
+            <div class="absolute inset-0 z-0 bg-black opacity-25"></div>
+            <div class="z-10 w-full px-24 font-black mt-11">
+                              {{-- 'nunito', 'montserrat','oswald','poppins','roboto' --}}
+                <h1 class="text-6xl font-roboto tracking-wide bg-black bg-opacity-5 italic">Search Accomodation</h1>
+                <p class="my-4 text-3xl font-roboto text-ump-green bg-black bg-opacity-30 italic">Browse your rental house in more secure way, anywhere.</p>
             </div>
             <div class="absolute bottom-0 left-0 right-0 flex justify-center p-4 space-x-4 text-center">
-                <span>
-                    <svg fill="#fff" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"/></svg>
-                </span>
-                <span>
-                    <svg fill="#fff" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"/></svg>
-                </span>
-                <span>
-                    <svg fill="#fff" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/></svg>
-                </span>
+
             </div>
         </div>
         <div class="z-0 flex items-center justify-center w-full px-0 text-center lg:w-1/2 md:px-16" style="background-color: #161616;">
             <div class="absolute inset-0 z-10 items-center bg-gray-500 bg-no-repeat bg-cover lg:hidden" >
                 <div class="absolute inset-0 z-0 bg-black opacity-60"></div>
+
             </div>
-            <div class="z-20 w-full py-6">
-                <h1 class="my-6">
-                    UMP Off-Campus Housing
-                </h1>
-                <div class="py-6 space-x-2">
-                    <span class="inline-flex items-center justify-center w-10 h-10 text-lg font-bold border-2 border-white rounded-full">f</span>
-                    <span class="inline-flex items-center justify-center w-10 h-10 text-lg font-bold border-2 border-white rounded-full">G+</span>
-                    <span class="inline-flex items-center justify-center w-10 h-10 text-lg font-bold border-2 border-white rounded-full">in</span>
-                </div>
-                <p class="text-gray-100">
-                    or use email your account
-                </p>
-                <form action="" class="w-full px-4 mx-auto sm:w-2/3 lg:px-0">
-                    <div class="pt-4 pb-2">
-                        <input type="email" name="email" id="email" placeholder="Email" class="block w-full p-4 text-lg bg-black rounded-sm">
+            <div class="absolute top-0 flex justify-right p-4 space-x-4    ">
+
+                <a href="#" class="flex items-center flex-shrink-0 px-5 py-2 border-b-4 hover:text-indigo-400 ">List Your Property</a>
+                <a href="#" class="flex items-center flex-shrink-0 px-5 py-2 border-b-4 hover:text-indigo-400 ">Search Rental House</a>
+            </div>
+
+            <div class="z-auto w-full">
+
+                     <div class="grid grid-row-2 grid-flow-col gap-x-2.5 ">
+                        <div class="  row-span-2 justify-self-end animate-bounce">
+                            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                            width="100" height="100"
+                            viewBox="0 0 172 172"
+                            style=" fill:#000000;"><g transform="translate(2.408,2.408) scale(0.972,0.972)"><g fill="none" fill-rule="nonzero" stroke="none" stroke-width="none" stroke-linecap="butt" stroke-linejoin="none" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><g fill="#6366f1" stroke="#6366f1" stroke-width="5" stroke-linejoin="round"><path d="M88.10969,4.50828l32.63297,25.39688v-5.82516c0.00019,-1.89978 1.54022,-3.43981 3.44,-3.44h13.41735c1.89978,0.00019 3.43981,1.54022 3.44,3.44v20.64c0.00404,0.30353 -0.0321,0.60627 -0.1075,0.90031l26.29719,20.46531c1.0242,0.73488 1.56448,1.97105 1.40815,3.22188c-0.15632,1.25084 -0.98421,2.31602 -2.15777,2.77623c-1.17355,0.46022 -2.50483,0.24176 -3.46976,-0.56937l-4.77031,-3.71547v90.4411c-0.00019,1.89978 -1.54022,3.43981 -3.44,3.44h-47.61578c-0.37149,0.0614 -0.75054,0.0614 -1.12203,0h-40.15797c-0.37149,0.0614 -0.75054,0.0614 -1.12203,0h-47.58219c-1.89978,-0.00019 -3.43981,-1.54022 -3.44,-3.44v-90.4411l-4.77031,3.71547c-0.96493,0.81112 -2.2962,1.02958 -3.46976,0.56936c-1.17355,-0.46022 -2.00144,-1.52539 -2.15776,-2.77623c-0.15632,-1.25083 0.38395,-2.487 1.40814,-3.22188l79.12,-61.57735c0.56731,-0.43985 1.258,-0.69122 1.97531,-0.71891c0.80932,-0.03199 1.60399,0.22258 2.24406,0.71891zM20.64,62.45078v92.34922h41.28v-61.37578c-0.16054,-1.00223 0.12968,-2.02421 0.79301,-2.79248c0.66333,-0.76827 1.63206,-1.20442 2.64699,-1.19174h41.28c1.89978,0.00019 3.43981,1.54022 3.44,3.44v61.92h41.28v-92.34922l-65.36,-50.86766zM127.62265,35.26l6.53735,5.0861v-12.8261h-6.53735zM68.8,154.8h34.4v-58.48h-34.4z"></path></g><path d="M0,172v-172h172v172z" fill="none" stroke="none" stroke-width="1" stroke-linejoin="miter"></path><g fill="#6366f1" stroke="none" stroke-width="1" stroke-linejoin="miter"><path d="M85.86563,3.78938c-0.71731,0.02769 -1.408,0.27906 -1.97531,0.71891l-79.12,61.57735c-1.02419,0.73488 -1.56447,1.97105 -1.40814,3.22188c0.15632,1.25083 0.98421,2.31601 2.15776,2.77623c1.17355,0.46022 2.50482,0.24176 3.46976,-0.56936l4.77031,-3.71547v90.4411c0.00019,1.89978 1.54022,3.43981 3.44,3.44h47.58219c0.37149,0.0614 0.75054,0.0614 1.12203,0h40.15797c0.37149,0.0614 0.75054,0.0614 1.12203,0h47.61578c1.89978,-0.00019 3.43981,-1.54022 3.44,-3.44v-90.4411l4.77031,3.71547c0.96493,0.81113 2.29621,1.02958 3.46976,0.56937c1.17355,-0.46022 2.00144,-1.5254 2.15777,-2.77623c0.15632,-1.25084 -0.38396,-2.487 -1.40815,-3.22188l-26.29719,-20.46531c0.0754,-0.29405 0.11154,-0.59678 0.1075,-0.90031v-20.64c-0.00019,-1.89978 -1.54022,-3.43981 -3.44,-3.44h-13.41735c-1.89978,0.00019 -3.43981,1.54022 -3.44,3.44v5.82516l-32.63297,-25.39687c-0.64007,-0.49632 -1.43474,-0.7509 -2.24406,-0.71891zM86,11.58313l65.36,50.86766v92.34922h-41.28v-61.92c-0.00019,-1.89978 -1.54022,-3.43981 -3.44,-3.44h-41.28c-1.01493,-0.01267 -1.98366,0.42347 -2.64699,1.19174c-0.66333,0.76827 -0.95355,1.79024 -0.79301,2.79248v61.37578h-41.28v-92.34922zM127.62265,27.52h6.53735v12.8261l-6.53735,-5.0861zM68.8,96.32h34.4v58.48h-34.4z"></path></g><path d="" fill="none" stroke="none" stroke-width="1" stroke-linejoin="miter"></path></g></g></svg>
+                        </div>
+                        <div class=" row-span-1 "> <h1 class="text-4xl text-ump-green font-oswald font-bold tracking-wide text-left">UMP </h1></div>
+                        <div class=" row-span-1 "><h1 class="text-4xl text-ump-green font-oswald font-bold tracking-wide text-left">Off-Campus Housing</h1></div>
+                      </div>
+
+
+
+                <form action="" class="w-full px-6 mx-auto sm:w-2/3 lg:px-0">
+                    <div class="pt-1 pb-2">
+                        <input type="email" name="email" id="email" placeholder="Email or Username" required class="block w-full p-4 text-lg bg-black rounded-sm">
                     </div>
-                    <div class="pt-4 pb-2">
-                        <input class="block w-full p-4 text-lg bg-black rounded-sm" type="password" name="password" id="password" placeholder="Password">
+                    <div class="pt-1 pb-2">
+                        <input class="block w-full p-4 text-lg bg-black rounded-sm" type="password" required name="password" id="password" placeholder="Password">
                     </div>
+                  <div class="pt-1 pb-2">
+                        <select class="block w-full p-4 text-lg bg-black rounded-sm" name="role" required>
+                            <option value="">Choose User Type</option>
+                            <option value="Customer">Customer</option>
+                            <option value="Staff">Staff</option>
+                            <option value="Rider">Rider</option>
+                        </select>
+                    </div>
+
+
+
                     <div class="text-right text-gray-400 hover:underline hover:text-gray-100">
                         <a href="#">Forgot your password?</a>
                     </div>
                     <div class="px-4 pt-4 pb-2">
-                        <button class="block w-full p-4 text-lg uppercase rounded-full bg-ump-green hover:bg-indigo-600 focus:outline-none">sign in</button>
+                        <button class="block w-full p-4 text-lg uppercase rounded-full  bg-indigo-500 hover:bg-indigo-600 focus:outline-none">sign in</button>
                     </div>
 
-                    <div class="left-0 right-0 flex justify-center p-4 mt-16 space-x-4 text-center lg:hidden ">
-                        <a href="#">
-                            <svg fill="#fff" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"/></svg>
-                        </a>
-                        <a href="#">
-                            <svg fill="#fff" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"/></svg>
-                        </a>
-                        <a href="#">
-                            <svg fill="#fff" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/></svg>
-                        </a>
-                    </div>
                 </form>
+                <p class="px-6 font-nunito text-center ">Don't have an account yet?
+                    <a href="#" class="hover:underline hover:text-indigo-400">Sign up</a>.
+                </p>
             </div>
+
+
+                    <footer class="px-5 py-4 border-t footer bg-base-200 text-base-content border-base-300 absolute bottom-0 flex justify-center p-4 space-x-4 text-center ">
+                        <div>
+                             <p>Copyright © 2021 - All right reserved by Weeyc</p>
+                        </div>
+                     </footer>
+
         </div>
     </section>
 </body>
--- tailwind.config.js
+++ tailwind.config.js
@@ -20,15 +20,51 @@ module.exports = {
           DEFAULT: '#c0ccda',
           light: '#e0e6ed',
           green: '#15a195',
-        }
+        },
+
+		    transparent: 'transparent',
+			current: 'currentColor',
+			black: colors.black,
+			white: colors.white,
+			rose: colors.rose,
+			pink: colors.pink,
+			fuchsia: colors.fuchsia,
+			purple: colors.purple,
+			violet: colors.violet,
+			indigo: colors.indigo,
+			blue: colors.blue,
+			lightBlue: colors.lightBlue, // Only in Tailwind CSS <=v2.1
+			sky: colors.sky, // As of Tailwind CSS v2.2, `lightBlue` has been renamed to `sky`
+			cyan: colors.cyan,
+			teal: colors.teal,
+			emerald: colors.emerald,
+			green: colors.green,
+			lime: colors.lime,
+			yellow: colors.yellow,
+			amber: colors.amber,
+			orange: colors.orange,
+			red: colors.red,
+			warmGray: colors.warmGray,
+			trueGray: colors.trueGray,
+			gray: colors.gray,
+			blueGray: colors.blueGray,
+			coolGray: colors.coolGray,
       },
 
     extend: {
 
         backgroundImage: {
 
-            'pekan': "url('/images/UMP/wide.jpg')",
+           // 'pekan': "url('/images/UMP/wide.jpg')",
 
+
+           },
+
+           fontFamily: {
+               'poppins': 'Poppins',
+               'montserrat': 'Montserrat',
+               'oswald': 'Oswald',
+               'roboto': 'Roboto',
 
            }
     },
